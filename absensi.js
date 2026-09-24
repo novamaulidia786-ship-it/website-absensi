020cd562c7927a51d56dfb396c5d7e2e978c0e13
novamaulidia786-ship-it <novamaulidia786@gmail.com>
2026-09-24 23:10:29 +0700

    Update absensi.js

--- absensi.js
+++ absensi.js
@@ -43,7 +43,7 @@ function tampilkanData() {
 
     tabel.innerHTML = "";
 
-    dataAbsen.forEach((data, index) => {
+    mahasiswa.forEach((data, index) => {
 
         const row = document.createElement("tr");
 
